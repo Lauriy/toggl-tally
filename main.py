89327c3497841c30b2576e3c00e9b0046320db93
Lauri Elias <laurileet@gmail.com>
2018-06-07 17:10:09 +0300

    Round in seconds conversion, don't floor

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from csv import reader
 data = {}
 header_row = None
 total = 0
-with open('Toggl_time_entries_2017-10-01_to_2017-10-31.csv', 'r', encoding='utf8') as f:
+with open('Toggl_time_entries_2018-05-01_to_2018-05-31.csv', 'r', encoding='utf8') as f:
     for row in reader(f, delimiter=','):
         if not header_row:
             header_row = row
@@ -11,7 +11,7 @@ with open('Toggl_time_entries_2017-10-01_to_2017-10-31.csv', 'r', encoding='utf8
         row = dict(zip(header_row, row))
         duration = row.get('Duration')
         duration_parts = duration.split(':')
-        duration = int(duration_parts[0]) * 60 + int(duration_parts[1]) + int(float(duration_parts[2]) / 60.0)
+        duration = int(duration_parts[0]) * 60 + int(duration_parts[1]) + round(float(duration_parts[2]) / 60.0)
         description = row.get('Description')
         if description in data:
             data[description] += duration
